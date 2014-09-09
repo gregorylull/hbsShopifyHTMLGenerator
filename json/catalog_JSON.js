5aa6a41eeae58de80f8a146c7b3aefbf1161088c
gregorylull <gregorylull@gmail.com>
2014-09-09 04:25:31 -0700

    finished products / collections settings HTML

---
+++ json/catalog_JSON.js
@@ -0,0 +1,18 @@
+window.myApp.currentJSON = JSON.stringify(
+{
+  "_settings": {
+    
+  },
+  "_data": {
+    "products_frame_blurb_header_fontSize": "3em",
+    "products_frame_blurb_text_fontSize": "3em",
+    "products_frame_menu_header_fontSize": "3em",
+    "products_frame_menu_item_fontSize": "3em",
+    "products_grid_header_fontSize": "3em",
+    "products_grid_header_current_fontSize": "3em",
+    "products_grid_product_name_fontSize": "3em",
+    "products_grid_product_price_fontSize": "3em",
+    "products_grid_product_price_compare_fontSize": "3em"
+  }
+}
+);
