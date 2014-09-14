684e8b56873df3729ecf90f1979cc672047ee02a
gregorylull <gregorylull@gmail.com>
2014-09-14 12:59:43 -0700

    finished products page / catalog-frame Header and Content settings.html input

--- js/products-collections.js
+++ js/products-collections.js
@@ -11,8 +11,30 @@ var app = window.myApp;
 
 app.createSettingsHTML = function () {
   var fieldset = document.createElement('fieldset');
-  fieldset.appendChild(app.createLegend('Products / Collections Content');
-  app.createSmall("The content here will show up on the products page and the individual-product page as a HBS blurb", fieldset);
+  fieldset.appendChild(app.createLegend('Products / Collections Content'));
+  app.createSmall("The content here will show up on the products page and the individual-product page in the left column as a header and blurb", fieldset);
+  var table = app.createTable();
+  table.appendChild(app.createHeader("Products Page: Left Column Content"));
+
+  var base = "products_blurb_";
+  
+  // create a header
+  var blurbHeader = app.createTableRow(
+      app.createLabel(base + 'header', 'Page header:'),
+      app.createInput('text', base + 'header', base + 'header')
+    );
+  table.appendChild(blurbHeader);
+
+  // create a blurb 
+   var blurbHeader = app.createTableRow(
+      app.createLabel(base + 'content', 'Page blurb:'),
+      app.createTextArea(base + 'content', base + 'content')
+    );
+  table.appendChild(blurbHeader); 
+
+  // FIELDSET!!
+  fieldset.appendChild(table);
+  return fieldset;
 };
 
 $(function () {
--- json/products-collections_JSON.js
+++ json/products-collections_JSON.js
@@ -4,6 +4,8 @@ window.myApp.currentJSON = JSON.stringify(
     
   },
   "_data": {
+    "products_blurb_content": "The Student Association Office offers a variety of products for HBS students to make their experience even better. Our Product Managers (who are also HBS Partners!) are located right across from The Grille in Spangler, and are always happy to field any questions. The office is open Monday to Thursday from 10am to 4pm. They can also be reached by phone at 617-496-6854.",
+    "products_blurb_header": "HBS SA Products"
   }
 }
 );
